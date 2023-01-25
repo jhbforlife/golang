0099134a5b65f7e49c474c6b3b0ccb616f0578fa
Jacob Brown <jacob.brown@shipt.com>
2023-01-25 11:26:43 -0600

    Finished client

--- cmd/capstone/client/main.go
+++ cmd/capstone/client/main.go
@@ -7,17 +7,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 const serviceURL = "https://translate-jmmhwrz5fq-uc.a.run.app/json"
 
 func main() {
-	from := flag.String("from", "", "language to translate from")
-	to := flag.String("to", "", "language to translate to")
+	flag.Usage = func() {
+		w := flag.CommandLine.Output()
+		fmt.Fprint(w, "\nusage: translate [options] text\n\n-f, -from\n\tlanguage to translate from\n-h, -help\n\tprint this message\n-t, -to *required\n\tlanguage to translate to\n\n")
+	}
+
+	fFlag := flag.String("f", "", "language to translate from")
+	fromFlag := flag.String("from", "", "language to translate from")
+	hFlag := flag.Bool("h", false, "display help message")
+	helpFlag := flag.Bool("help", false, "display help message")
+	tFlag := flag.String("t", "", "language to translate to")
+	toFlag := flag.String("to", "", "language to translate to")
 	flag.Parse()
 
-	if *to == "" {
+	if *hFlag || *helpFlag {
+		flag.Usage()
+		return
+	}
 
+	from := getStringFromFlags(*fFlag, *fromFlag)
+	to := getStringFromFlags(*tFlag, *toFlag)
+
+	if to == "" {
+		fmt.Println("translate: no to language specified")
+		flag.Usage()
+		return
 	}
 
 	var text string
@@ -25,31 +45,52 @@ func main() {
 		text += arg
 	}
 
-	if text == "" {
+	if len(strings.Fields(text)) == 0 {
+		fmt.Println("translate: no text to translate")
 		flag.Usage()
 		return
 	}
 
-	bs, err := json.Marshal(map[string]string{"from": *from, "to": *to, "text": text})
-	if err != nil {
-
+	bs, err := json.Marshal(map[string]string{"from": from, "to": to, "text": text})
+	if checkErr(err) {
+		return
 	}
+
 	req, err := http.NewRequest(http.MethodPut, serviceURL, bytes.NewReader(bs))
-	if err != nil {
-		panic(err)
+	if checkErr(err) {
+		return
 	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-
+	if checkErr(err) {
+		return
 	}
 	defer resp.Body.Close()
 
 	rs, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
+	if checkErr(err) {
+		return
 	}
-	fmt.Printf("translate \"%s\" to %s: %s\n", text, *to, string(rs))
+
+	fmt.Printf("translate \"%s\" to %s: %s\n", text, to, string(rs))
+}
+
+func getStringFromFlags(f, g string) string {
+	if len(strings.Fields(f)) == 0 && len(strings.Fields(g)) != 0 {
+		return g
+	} else if len(strings.Fields(f)) != 0 && len(strings.Fields(g)) == 0 {
+		return f
+	} else {
+		return ""
+	}
+}
+
+func checkErr(err error) bool {
+	if err != nil {
+		fmt.Printf("translate: %v\n", err)
+		return true
+	}
+	return false
 }
